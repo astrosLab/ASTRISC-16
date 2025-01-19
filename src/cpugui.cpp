966287de4ede83bdbbe35c3eef449bd10e85d2b9
Anthony P <aprosser2010@gmail.com>
2025-01-18 19:13:51 -0500

    Slightly modified GUI

--- src/cpugui.cpp
+++ src/cpugui.cpp
@@ -11,12 +11,14 @@ CpuWindow::CpuWindow() {
 		std::cout << "Unable to load font.\n";
 	}
 
-	for (int i = 0; i < 8; ++i) {
-		m_registers[i].setCharacterSize(24);
-		m_registers[i].setFont(m_sevensegment);
-		m_registers[i].setFillColor(Color::Black);
-		m_registers[i].setPosition(i * 100 + 500, 200);
+	if (!m_jetbrains.loadFromFile("./fonts/jetbrains.ttf")) {
+		std::cout << "Unable to load font.\n";
 	}
+
+	m_registers.setFont(m_jetbrains);
+	m_registers.setCharacterSize(24);
+	m_registers.setFillColor(Color::Black);
+	m_registers.setPosition({1000, 20});
 }
 
 void CpuWindow::update(int registers[8]) {
@@ -29,11 +31,18 @@ void CpuWindow::update(int registers[8]) {
 
 	m_window.clear(sf::Color::White);
 
-	for (int i = 0; i < 8; ++i) {
-		m_registers[i].setString(std::to_string(registers[i]));
-		m_window.draw(m_registers[i]);
-		std::string reg = m_registers[i].getString();
-	}
+	std::stringstream ss;
+	ss 
+		<< "R0: " << addPadding(std::to_string(registers[0]), 5, ' ')
+		<< "\nR1: " << addPadding(std::to_string(registers[1]), 5, ' ')
+		<< "\nR2: " << addPadding(std::to_string(registers[2]), 5, ' ')
+		<< "\nR3: " << addPadding(std::to_string(registers[3]), 5, ' ')
+		<< "\nR4: " << addPadding(std::to_string(registers[4]), 5, ' ')
+		<< "\nR5: " << addPadding(std::to_string(registers[5]), 5, ' ')
+		<< "\nR6: " << addPadding(std::to_string(registers[6]), 5, ' ')
+		<< "\nR7: " << addPadding(std::to_string(registers[7]), 5, ' ');
+	m_registers.setString(ss.str());
+	m_window.draw(m_registers);
 
 	RectangleShape rect({940, 1045});
 	rect.setPosition(20, 20);
@@ -55,7 +64,8 @@ void CpuWindow::update(int registers[8]) {
 
 	Text numbers;
 	numbers.setPosition({45, 40});
-	numbers.setString(std::to_string(registers[0]));
+	
+	numbers.setString(addPadding(std::to_string(registers[0]), 5, '0'));
 	numbers.setFont(m_sevensegment);
 	numbers.setCharacterSize(55);
 	numbers.setFillColor(Color::White);
@@ -66,5 +76,9 @@ void CpuWindow::update(int registers[8]) {
 	m_window.draw(numbers);
 
 	m_window.display();
+}
+
+std::string CpuWindow::addPadding(std::string inital, int width, char paddingChar) {
+	return inital.insert(0, width > inital.length() ? width - inital.length() : 0, paddingChar);
 }
 
--- src/cpugui.h
+++ src/cpugui.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <array>
 #include <string>
+#include <sstream>
 
 using namespace sf;
 
@@ -13,11 +14,13 @@ public:
 	CpuWindow();
 
 	void update(int registers[8]);
+	std::string addPadding(std::string inital, int width, char paddingChar);
 	bool isWindowOpen() { return m_window.isOpen(); }
 private:
 	RenderWindow m_window;
 	Font m_sevensegment;
-	std::array<Text, 8> m_registers;
+	Font m_jetbrains;
+	Text m_registers;
 };
 
 #endif // CPUGUI_H
